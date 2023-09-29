52c1bbadac4ef668898b56bc5e12e34dd84ccfe7
serj.kolbey <serj.kolbey@kiv.dev>
2023-09-29 20:22:13 +0600

    feat: change /src folder to /source, add configuration for starting up index.js from /soruce (TM-01)

---
+++ config-overrides.js
@@ -0,0 +1,5 @@
+const paths = require('react-scripts/config/paths')
+const path = require('path')
+
+paths.appSrc = path.resolve(__dirname, 'source')
+paths.appIndexJs = path.resolve(__dirname, 'source/index.js')
